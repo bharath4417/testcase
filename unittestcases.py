f1d806102d2c37b8107ad651509d689d15d36683
bharath <149502497+bharath4417@users.noreply.github.com>
2024-09-21 11:24:21 +0530

    Update unittestcases.py

--- unittestcases.py
+++ unittestcases.py
@@ -20,5 +20,17 @@ def test_3(self):
   
     self.assertEqual(result, 12)
 
+def test_4(self):
+
+    result = multiplication(-3, 4)
+  
+    self.assertEqual(result, 12)
+
+def test_3(self):
+
+    result = multiplication(-1, -1)
+  
+    self.assertEqual(result, 1)
+
 if __name__ == '__main__':
   unittest.main()
